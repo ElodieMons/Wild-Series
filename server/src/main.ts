3c58c56071c8c7e3c59456e650e61a85504b647a
ElodieMons <emons47110@gmail.com>
2025-01-06 11:38:48 +0100

    Q02 - Part1

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,6 +1,9 @@
 // Load environment variables from .env file
 import "dotenv/config";
 
+// import type RequestHandler
+import type { RequestHandler } from "express";
+
 // Check database connection
 // Note: This is optional and can be removed if the database connection
 // is not required when starting the application
@@ -9,8 +12,16 @@ import "../database/checkConnection";
 // Import the Express application from ./app
 import app from "./app";
 
+// Road declaration
+const sayWelcome: RequestHandler = (_req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+// Register Road
+app.get("/", sayWelcome);
+
 // Get the port from the environment variables
-const port = process.env.APP_PORT;
+const port = process.env.APP_PORT || 3310;
 
 // Start the server and listen on the specified port
 app
--- server/src/router.ts
+++ server/src/router.ts
@@ -9,6 +9,7 @@ const router = express.Router();
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
 
+router.get("/");
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
